055a1a57f2ccdfbb68f370230482ab0c2d57c60a
Umar Shaikh <umarhabib464@gmail.com>
2021-12-09 16:58:25 +0500

    50%

---
+++ app.js
@@ -0,0 +1,17 @@
+function modalfunc(x){
+    //GETTING ELEMENTS
+    var imageofmodal = document.getElementById("imageofmodal")
+    var  prodName = document.getElementById("prodName")
+    var  prodPrice = document.getElementById("prodPrice")
+
+    //INCLUDING REQUIREMENTS
+    var prod = x.parentNode.firstElementChild.innerHTML
+    var price = x.parentNode.firstElementChild.nextElementSibling.innerHTML
+    var img = x.parentNode.parentNode.firstElementChild
+
+    //SETTING ELEMENTS    
+    prodName.innerHTML = prod
+    prodPrice.innerHTML = price
+    // img.src = imageofmodal
+    imageofmodal.src = img.src
+}
