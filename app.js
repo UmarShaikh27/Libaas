c16319686e59211c98e8a84b07ce6f5d62bdf1fa
Umar Shaikh <umarhabib464@gmail.com>
2021-12-11 22:21:18 +0500

    seventy

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ function modalfunc(x){
     var  prodPrice = document.getElementById("prodPrice")
 
     //INCLUDING REQUIREMENTS
-    var prod = x.parentNode.firstElementChild.innerHTML
-    var price = x.parentNode.firstElementChild.nextElementSibling.innerHTML
-    var img = x.parentNode.parentNode.firstElementChild
+    var prod = x.nextElementSibling.firstElementChild.innerHTML
+    var price = x.nextElementSibling.firstElementChild.nextElementSibling.innerHTML
+    var img = x.parentNode.firstElementChild
 
     //SETTING ELEMENTS    
     prodName.innerHTML = prod
@@ -15,6 +15,21 @@ function modalfunc(x){
     // img.src = imageofmodal
     imageofmodal.src = img.src
 }
-function prodfunc(x){
-    x.parentNode.classList.add("active")
+function minusfunc(x){
+    var modalCounter = document.getElementById("modalCounter")
+    var counterValue = Number(modalCounter.firstElementChild.nextElementSibling.innerHTML) 
+    if(counterValue > 1){
+    modalCounter.firstElementChild.nextElementSibling.innerHTML = counterValue - 1
+    }
+    
+   
+    
+}
+function plusfunc(x){
+    var modalCounter = document.getElementById("modalCounter")
+    var counterValue = Number(modalCounter.firstElementChild.nextElementSibling.innerHTML)
+    if(counterValue < 10 ){
+        modalCounter.firstElementChild.nextElementSibling.innerHTML = counterValue + 1
+    }
+    
 }
